bb47decb0979c6d264f852cd4ccf86ae7ddeec61
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2024-06-21 19:14:29 +0500

    function 5

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
     print_labeled_measurement(5, 'h');
 
-    println!("The value is: {}", five());
+    let value = five();
+    let new_value = plus_one(value);
+
+    println!("The value is: {}", value);
+    println!("The value plus one is: {}", new_value);
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
 
 fn five() -> i32 {
