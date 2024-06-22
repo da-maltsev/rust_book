0e7ff50f865e0378d75a29d1509b59ba22965295
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2024-06-22 14:17:03 +0500

    branches 1

---
+++ branches/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let number = None;
+
+    if number < 5 {
+        println!("number is less");
+    } else if number > 4  {
+        println!("number is bigger");
+    } else {
+        println!("number was something other than zero");
+    }
+}
