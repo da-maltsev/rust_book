7ca85634e74435bcb7c59b234b7b39eb1c735a5c
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2024-06-22 14:17:30 +0500

    branches 2

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
-    let number = None;
+    let number = 6;
 
-    if number < 5 {
-        println!("number is less");
-    } else if number > 4  {
-        println!("number is bigger");
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
     } else {
-        println!("number was something other than zero");
+        println!("number is not divisible by 4, 3, or 2");
     }
 }
